5ef9cb35f02421de4edc58e2fcd35d0385aeaac9
root <root@p-8d26ac2d-2fa9-4c64-a68e-d7c95896b6ff.projects-domain.projects.svc.cluster.local>
2023-04-02 06:23:41 +0000

    initial commit

--- pages/About-Me.py
+++ pages/About-Me.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="About Me",
 )
 st.title('About Me')
-st.image('Strava-Analyzer/pages/Steps/Image (4).jpeg',width=400)
+st.image('pages/Steps/Image (4).jpeg',width=400)
 st.header('My Background')
 st.write('My name is Cole Duval. I’m currently 21 years old, and I currently go to Bentley university studying to get my undergraduate in computer information systems. I am on the cross-country team at Bentley, and I’m also in several clubs. I love getting outside and being active, and my favorite activities include running, coding, and playing the guitar.')
 st.header('My Goals for this Website')
